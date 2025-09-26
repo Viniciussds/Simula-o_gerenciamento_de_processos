5686bdc1f61119c926d3347562d6fcc7cc6abb8f
DaviTitoT <168222372+DaviTitoT@users.noreply.github.com>
2025-09-25 21:03:30 -0300

    Corrigindo fifo.py

--- Model/FIFO/fifo.py
+++ Model/FIFO/fifo.py
@@ -12,9 +12,14 @@ class Fifo:
         processos = sorted(self.todos_processos, key=lambda p: p.get_tempo_chegada())
 
         while len(self.processos_finalizados) < len(processos):
+            # Adiciona novos processos à fila apenas uma vez
             for p in processos:
-                if p.get_tempo_chegada() <= self.tempo_atual and p.get_estado_processo() == "Pronto":
+                if (
+                    p.get_tempo_chegada() <= self.tempo_atual 
+                    and p.get_estado_processo() == "Pronto"
+                ):
                     self.fila_processos.push(p)
+                    p.set_estado_processo("NaFila")  # marca que já foi enfileirado
 
             if self.fila_processos.eVazia():
                 self.tempo_atual += 1
@@ -22,11 +27,14 @@ class Fifo:
 
             no = self.fila_processos.pop()
             processo = no.data
+
             processo.set_estado_processo("Executando")
             tempo_inicio = self.tempo_atual
+
             wt = tempo_inicio - processo.get_tempo_chegada()
             self.tempo_atual += processo.get_tempo_execucao()
             tt = self.tempo_atual - processo.get_tempo_chegada()
+
             processo.set_estado_processo("Finalizado")
             self.processos_finalizados.append(processo)
 
